fe07d7bb5d4fe6fc8df8b0dd2c7b10e17cce6609
Vasis <vasis123@gmail.com>
2012-03-23 18:30:20 +0400

    Adding Russian translation

---
+++ core/components/emailresource/lexicon/ru/properties.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * EmailResource
+ *
+ *
+ *
+ * EmailResource is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * EmailResource is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * EmailResource; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package emailresource
+ */
+/**
+ * Russian Lexicon Entries for EmailResource
+ *
+ * by vasis123 (http://anidev.ru)
+ *
+ * @package emailresource
+ * @subpackage lexicon
+ */
+
+/* EmailResource Property Description strings */
+$_lang['er_mail_from_desc'] = '(опционально) MAIL_FROM - настройка для отправляемых email\'ов. По умолчанию используется emailsender из Системных Настроек';
+$_lang['er_mail_from_name_desc'] = '(опционально) MAIL_FROM_NAME - настройка для отправляемых email\'ов. По умолчанию используется site_name из Системных Настроек.';
+$_lang['er_mail_sender_desc'] = '(опционально) EMAIL_SENDER - настройка для отправляемых email\'ов. По умолчанию используется emailsender из Системных Настроек.';
+$_lang['er_mail_reply_to_desc'] = '(опционально) REPLY_TO - настройка для отправляемых email\'ов. По умолчанию используется emailsender из Системных Настроек';
+$_lang['er_mail_subject_desc'] = '(опционально) MAIL_SUBJECT - настройка для отправляемых email\'ов. По умолчанию используется расширенный заголовк ресурса, или название ресурса если расширенный заголовок пустой.';
+$_lang['er_template_list_desc'] = '(опционально но очень не рекомендуемо) Список разделенных запятыми  ID Шаблонов. Список всех шаблонов используемых ресурсами, которые должны быть отправлены. Это увеличивает скорость работы сайта, предотвращая запуск плагина для страниц, которые не нужно отсылать по электронной почте.';
---
+++ core/components/emailresource/lexicon/ru/tvs.inc.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * EmailResource
+ *
+ *
+ *
+ * EmailResource is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * EmailResource is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * EmailResource; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package emailresource
+ */
+/**
+ * Russian Lexicon Entries for EmailResource
+ *
+ * by vasis123 (http://anidev.ru)
+ *
+ * @package emailresource
+ * @subpackage lexicon
+ */
+
+/* EmailResource TV Description strings */
+$_lang['er_preview_email_desc'] = 'Показывать превью email\'а при выводе превью ресурса.';
+$_lang['er_email_on_preview_desc'] = 'Отправить тествое сообщение ресурса когда показывается превью.';
+$_lang['er_send_test_email_desc'] = 'Отправить ресурс для всех подписчиков когда показывается превью.';
+$_lang['er_email_address_for_test_desc'] = 'Адрес электронной почты для отправки тестовых сообщений.';
+$_lang['er_css_mode_desc'] = 'Определяет, как CSS хранится.';
+$_lang['er_css_files_desc'] = 'Список разделенных запятыми файлов CSS, чанков, или имен ресурсов. Для файлов, CssBasePath будет добавлен.';
+$_lang['er_inline_css_desc'] = 'Использовать CSS файлы для создания встроенного CSS';
+$_lang['er_css_base_path_desc'] = 'Путь к директрии содержащей файлы CSS (игнорируется если режим установлен в РЕСУРС или ЧАНК) -- должен заканчиваться косой чертой ( / ).';
+$_lang['er_groups_desc'] = 'Список разделенных запятыми групп которым отправлять';
+$_lang['er_tags_desc'] = 'Список разделенных запятыми тэгов; если установлено, только те пользователи, у которых установлен один из этих тэгов в поле комментариев будут получать электронную почту; по умолчанию: (пусто).';
+$_lang['er_batch_size_desc'] = 'Размер каждой партии для отправки; по умолчанию: 50';
+$_lang['er_batch_delay_desc'] = 'Задержка между партиями (в секундах); по умолчанию: 1 сек';
+$_lang['er_item_delay_desc'] = 'Задержка между каждым email\'ом (в секундах); по умолчанию: .51 сек';
+
+
+ 
