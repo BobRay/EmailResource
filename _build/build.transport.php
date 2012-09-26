eadfecae756b640b04f3066d17e10dfd608c6cc4
bobray <bobray@softville.com>
2012-09-26 01:37:08 -0500

    bumped version to 1.1.3-pl

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -31,8 +31,8 @@
 /* Set package info be sure to set all of these */
 define('PKG_NAME','EmailResource');
 define('PKG_NAME_LOWER','emailresource');
-define('PKG_VERSION','1.1.2');
-define('PKG_RELEASE','rc2');
+define('PKG_VERSION','1.1.3');
+define('PKG_RELEASE','pl');
 define('PKG_CATEGORY','EmailResource');
 
 /* Set package options - you can turn these on one-by-one
