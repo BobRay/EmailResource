20a4b066b208f514b9ed735c361010eaba39490d
bobray <bobray@softville.com>
2017-03-29 23:15:14 -0500

    First working test version

--- _build/test/testFullUrls.php
+++ _build/test/testFullUrls.php
@@ -15,16 +15,104 @@
  * @package emailResource
  **/
 
+function convert($base, $url, $protocol, $domain) {
 
-function fullUrls($base, $html) {
+
+     echo "\nReceived:" . $url;
+
+     $base = rtrim($base, '/');
+     if (preg_match('@https?@', $url)) {
+         return $url;
+     }
+
+     if (preg_match('@^[\s]*//@', $url)) {
+         return $url;
+     }
+
+     if (preg_match('@^[\s]*#@', $url)) {
+         return $url;
+     }
+
+     /* $url has no protocol beyond this point */
+
+
+    $url = ltrim($url, '/');
+    $ourDomain = strpos($url, $domain) !== false;
+    $hasAnyDomain  = preg_match('@^[^\/]*\..+\/@', $url);
+    $www = 'www.';
+    $UrlHasWww = stripos($url, 'www.') !== false;
+    $baseHasWww = stripos($base, 'www.') !== false;
+
+    if (! $UrlHasWww && ! $baseHasWww) {
+
+    }
+
+
+
+    /* contains our domain name but no protocol*/
+     if ($ourDomain) {
+         $url =  $protocol . $url;
+     } elseif (!$hasAnyDomain) {
+
+     /* No domain - must be ours */
+
+         $url = $base . '/' . $url;
+     } else {
+         $url = 'http://' . $url;
+     }
+     /*if (strpos($url, $domain) === false) {
+
+       $url = $base . '/' . $url;
+
+        // return preg_replace('@(.*?href[\s]*\=[\s]*[\'"])([\/])*(.*)@', '\1' . $base . '/\3',$url);
+
+     }*/
+
+     return $url;
+ }
+
+function fullUrls($base, $url) {
+
+
     /* Extract domain name and protocol from $base */
     $splitBase = explode('//', $base);
-    $protocol = $splitBase[0];
+    $protocol = $splitBase[0] . '//';
     $domain = $splitBase[1];
     $domain = rtrim($domain, '/ ');
 
+    echo "\nProtocol: " . $protocol;
+    echo "\nBase: " . $base;
+    echo "\nDomain: " . $domain;
+
+
+    $pattern = '@<(?:a|img)[\s]+(?:href|src)[^>]+\>@i';
+    preg_match($pattern, $url, $matches);
+
+    // echo "\n\n" . print_r($matches, true) . "\n\n";
+
+    /* $fullTag is only the a part:
+            <a href="somethings">
+      or    <img src="something"> */
+
+    $fullTag = $matches[0];
+    echo "\nFullTag: " . $fullTag;
+
+    /* extract URL from tag */
+    $pattern2 = '@(?:href|src)[\s]*=[\s]*[\'\"]([^\'"]+)[\'\"][\s]*>@';
+    preg_match($pattern2, $fullTag, $matches);
+    $originalUrl = $matches[1];
+    echo "\nOriginalUrl: " . $originalUrl;
+
+    $newUrl = convert($base, $originalUrl, $protocol, $domain);
+
+    echo "\nReturned: " , $newUrl;
+    if ($originalUrl !== $newUrl) {
+        $fullTag = str_replace($originalUrl, $newUrl, $fullTag);
+    }
+
+
+     return $fullTag;
     /* remove space around = sign */
-    //$html = preg_replace('@(href|src)\s*=\s*@', '\1=', $html);
     $html = preg_replace('@(?<=href|src)\s*=\s*@', '=', $html);
 
     /* Fix google link weirdness */
@@ -39,6 +127,8 @@ function fullUrls($base, $html) {
     /* Correct base URL, if necessary */
     $server = preg_replace('@^([^\:]*)://([^/*]*)(/|$).*@', '\1://\2/', $base);
 
+    /* Protect external links with no protocol */
+
     /* Handle root-relative URLs */
     //$html = preg_replace('@\<([^>]*) (href|src)="/([^"]*)"@i', '<\1 \2="'.$server.'\3"', $html);
     $html = preg_replace('@\<([^>]*) (href|src)="/([^#"]*)"@i', '<\1 \2="' . $server . '\3"', $html);
@@ -47,10 +137,10 @@ function fullUrls($base, $html) {
     //$html = preg_replace('@\<([^>]*) (href|src)="(?!http|mailto|sip|tel|callto|sms|ftp|sftp|gtalk|skype)(([^\:"])*|([^"]*:[^/"].*))"@i', '<\1 \2="'.$base.'\3"', $html);
     $html = preg_replace('@\<([^>]*) (href|src)="(?!#|http|mailto|sip|tel|callto|sms|ftp|sftp|gtalk|skype)(([^\:"])*|([^"]*:[^/"].*))"@i', '<\1 \2="' . $base . '\3"', $html);
 
-    preg_match('@([a-zA-Z])(\/\/)([a-zA-Z])@', $html, $matches);
+    // preg_match('@([a-zA-Z])(\/\/)([a-zA-Z])@', $html, $matches);
 
     /* Remove double slashes in path */
-    $html = preg_replace('@([a-zA-Z])(\/\/)([a-zA-Z])@', '\1/\3', $html);
+    // $html = preg_replace('@([a-zA-Z])(\/\/)([a-zA-Z])@', '\1/\3', $html);
 
     return $html;
 }
@@ -70,13 +160,13 @@ $cases = array(
     // Case 1
     array(
         'initial' => '<a href="page1.html">page1.html</a>',
-        'expected' => '<a href="' . $basePlaceholder . 'page1.html">page1.html</a>'
+        'expected' => '<a href="' . $basePlaceholder . 'page1.html">'
     ),
 
     // Case 2
     array(
         'initial' => '<a href="section1/page1.html">section1/page1.html</a>',
-        'expected' => '<a href="' . $basePlaceholder . 'section1/page1.html">section1/page1.html</a>'
+        'expected' => '<a href="' . $basePlaceholder . 'section1/page1.html">'
     ),
 
     // Case 3
@@ -88,66 +178,78 @@ $cases = array(
     // Case 4
     array(
         'initial' => '<a href="/section1/page1.html">/section1/page1.html</a>',
-        'expected' => '<a href="' . $basePlaceholder . 'section1/page1.html">/section1/page1.html</a>',
+        'expected' => '<a href="' . $basePlaceholder . 'section1/page1.html">',
     ),
 
     // Case 5
     array(
         'initial' => '<a href="/section1/page1.html#jumpto">/section1/page1.html#jumpto</a>',
-        'expected' => '<a href="' . $basePlaceholder . 'section1/page1.html#jumpto">/section1/page1.html#jumpto</a>'
+        'expected' => '<a href="' . $basePlaceholder . 'section1/page1.html#jumpto">'
     ),
 
     // Case 6
     array(
-        'initial' => '<a href="http://www.externaldomain.com/page1.html">http://www.externaldomain.com/page1.html</a>',
-        'expected' => '<a href="http://www.externaldomain.com/page1.html">http://www.externaldomain.com/page1.html</a>'
+        'initial' => '<a href="http://www.external.com/page1.html">http://www.externaldomain.com/page1.html</a>',
+        'expected' => '<a href="http://www.external.com/page1.html">',
     ),
 
     // Case 7
     array(
-        'initial' => '<a href="www.externaldomain.com/page1.html">www.externaldomain.com/page1.html</a>',
-        'expected' => '<a href="' . $basePlaceholder . 'www.externaldomain.com/page1.html">www.externaldomain.com/page1.html</a>', //???
+        'initial' => '<a href="www.external.com/page1.html">www.externaldomain.com/page1.html</a>',
+        'expected' => '<a href="' . 'http://www.external.com/page1.html">',
     ),
 
     // Case 8
     array(
         'initial' => '<a href="www.domain.com/page1.html">www.domain.com/page1.html</a>',
         'expected' => '<a href="' .
-            $basePlaceholder . 'www.domain.com/page1.html">www.domain.com/page1.html</a>'
+            $basePlaceholder . 'page1.html">'
     ),
 
     // Case 9
     array(
-        'initial' => '<a href="//www.externaldomain.com/page1.html">//www.externaldomain.com/page1.html</a>',
-        'expected' => '<a href="//www.externaldomain.com/page1.html">//www.externaldomain.com/page1.html</a>',
+        'initial' => '<a href="//www.external.com/page1.html">//www.externaldomain.com/page1.html</a>',
+        'expected' => '<a href="//www.external.com/page1.html">',
     ),
 
     // Case 10
     array (
         'initial' => '<a href="#jumpto">#jumpto</a>',
-        'expected' => '<a href="#jumpto">#jumpto</a>'
+        'expected' => '<a href="#jumpto">'
     ),
 
     // Case 11
     array (
-        'initial' => '<a href="//www.externaldomain.com/page1.html">//www.externaldomain.com/page1.html</a>',
-        'expected' => '<a href="//www.externaldomain.com/page1.html">//www.externaldomain.com/page1.html</a>'
+        'initial' => '<a href="//www.external.com/page1.html">//www.externaldomain.com/page1.html</a>',
+        'expected' => '<a href="//www.external.com/page1.html">'
     ),
 
 
 );
 $i = 0;
+
+$lorum = "\n <p> Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit  </p>";
+$full = $lorum;
+$fullExpected = $lorum;
+
 foreach ($cases as $case) {
     $i++;
-    if (5 != $i) {
-        // continue;
+    if (8 != $i) {
+       // continue;
     }
     echo "\n\n[Case " . $i . "]";
 
     foreach ($bases as $base) {
         echo "\n\n**************************************************************";
         $initial = $case['initial'];
+        $full .= $initial . $lorum;
         $expected = str_replace($basePlaceholder, $base, $case['expected']);
+
+        /* correct Case 8 expected */
+        if ((strpos($initial, 'www.') !== false) && (strpos($expected, 'www.') === false)) {
+            $expected = str_replace('://', '://www.', $expected);
+        }
+        $fullExpected .= $expected . $lorum;
         $obtained = fullUrls($base, $initial);
         echo "\nInitial: " . $initial;
         echo "\nExpected:" . $expected;
@@ -155,6 +257,19 @@ foreach ($cases as $case) {
         echo ($expected === $obtained)? ' -- pass' : ' -- fail';
     }
 }
+
+/* preg_mactch_all */
+
+$pattern = '@<(?:a|img)[\s]+(?:href|src)[^>]+\>@i';
+preg_match_all($pattern, $full, $matches);
+
+echo "\n\nFULL:\n" . print_r($matches, true);
+
+$fullTags = $matches[0];
+
+echo "\n\nFULL:\n" . print_r($fullTags, true);
+// echo $full;
+
 
 exit;
 
